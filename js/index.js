2a464a8a86d6d10a24a5c9f5a5a817c0b311ed81
fxf199756 <you@example.com1054644545@qq.com>
2018-09-17 10:44:52 +0800

    修改

---
+++ js/index.js
@@ -0,0 +1,21 @@
+$(function () {
+    let push=$(".push");
+    let person=$(".list-person");
+    let flag=0;
+    person.click(function () {
+        if (flag==0){
+            flag=1;
+            push.css("display","block");
+            person.css("background","#F0F0F0");
+            person.children().last().attr("class","iconfont icon-shangjiantou");
+            return;
+        }
+        if (flag==1){
+            flag=0;
+            push.css("display","none");
+            person.css("background","white");
+            person.children().last().attr("class","iconfont icon-icon");
+            return;
+        }
+    })
+})
